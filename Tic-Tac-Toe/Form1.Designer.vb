bf875ab76cfe41880e1afc6e886c597730cd7b19
Johndell S. Kitts <k.jaydeee@gmail.com>
2022-05-23 14:31:00 +0800

    Form can now be dragged and move, and the close button now closes the form

--- Tic-Tac-Toe/Form1.Designer.vb
+++ Tic-Tac-Toe/Form1.Designer.vb
@@ -23,29 +23,62 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.titlePanel = New System.Windows.Forms.Panel()
+        Me.closeButton = New System.Windows.Forms.Button()
+        Me.mainPanel = New System.Windows.Forms.Panel()
+        Me.titlePanel.SuspendLayout()
         Me.SuspendLayout()
         '
         'titlePanel
         '
         Me.titlePanel.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.titlePanel.Controls.Add(Me.closeButton)
         Me.titlePanel.Dock = System.Windows.Forms.DockStyle.Top
         Me.titlePanel.Location = New System.Drawing.Point(0, 0)
         Me.titlePanel.Name = "titlePanel"
         Me.titlePanel.Size = New System.Drawing.Size(438, 34)
         Me.titlePanel.TabIndex = 0
         '
+        'closeButton
+        '
+        Me.closeButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.closeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.closeButton.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.closeButton.ForeColor = System.Drawing.Color.Red
+        Me.closeButton.Location = New System.Drawing.Point(403, 1)
+        Me.closeButton.Margin = New System.Windows.Forms.Padding(0)
+        Me.closeButton.Name = "closeButton"
+        Me.closeButton.Size = New System.Drawing.Size(26, 30)
+        Me.closeButton.TabIndex = 0
+        Me.closeButton.Text = "x"
+        Me.closeButton.TextAlign = System.Drawing.ContentAlignment.TopLeft
+        Me.closeButton.UseVisualStyleBackColor = False
+        '
+        'mainPanel
+        '
+        Me.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.mainPanel.Location = New System.Drawing.Point(0, 34)
+        Me.mainPanel.Name = "mainPanel"
+        Me.mainPanel.Size = New System.Drawing.Size(438, 469)
+        Me.mainPanel.TabIndex = 1
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(438, 503)
+        Me.Controls.Add(Me.mainPanel)
         Me.Controls.Add(Me.titlePanel)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MaximumSize = New System.Drawing.Size(438, 503)
+        Me.MinimumSize = New System.Drawing.Size(438, 503)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Tic-Tac-Toe"
+        Me.titlePanel.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents titlePanel As Panel
+    Friend WithEvents closeButton As Button
+    Friend WithEvents mainPanel As Panel
 End Class
--- Tic-Tac-Toe/Form1.vb
+++ Tic-Tac-Toe/Form1.vb
@@ -1,3 +1,23 @@
 ﻿Public Class Form1
+    Dim Pos As Point ''' to be used for drag and moving of form
+    ''' <summary>
+    ''' to make form to be dragged and move when using the top panel
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub titlePanel_MouseMove(sender As Object, e As MouseEventArgs) Handles titlePanel.MouseMove
+        If (e.Button = Windows.Forms.MouseButtons.Left) Then
+            Me.Location += Control.MousePosition - Pos
+        End If
+        Pos = Control.MousePosition
+    End Sub
 
+    ''' <summary>
+    ''' to close all open forms when clicked
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub closeButton_Click(sender As Object, e As EventArgs) Handles closeButton.Click
+        Me.Close()
+    End Sub
 End Class
