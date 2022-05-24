cfc941600e921b1cd9314d82181157993a058ae4
Johndell S. Kitts <k.jaydeee@gmail.com>
2022-05-24 20:52:46 +0800

    Added reset function so when you go back to main menu, the game will reset. It checks now if someone did win and show a messagebox of who won

--- Tic-Tac-Toe/GameArea.vb
+++ Tic-Tac-Toe/GameArea.vb
@@ -4,6 +4,10 @@
 Public Class GameArea
     Public player1Turn As Boolean = True
     Public buttons() As Button
+    Public player1Combinations As New List(Of Short)()
+    Public player2Combinations As New List(Of Short)()
+    Public winningCombination(,) As Short = {{11, 12, 13}, {21, 22, 23}, {31, 32, 33},
+        {11, 21, 31}, {12, 22, 32}, {13, 23, 33}, {11, 22, 33}, {13, 22, 31}}
 
     ''' <summary>
     ''' Load the buttons in the array when GameArea form loads
@@ -11,9 +15,35 @@ Public Class GameArea
     Private Sub GameArea_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         buttons = {t11, t12, t13, t21, t22, t23, t31, t32, t33}
     End Sub
+
+    ''' <summary>
+    ''' Clear the images on tiles and clear the combinations made by the players
+    ''' </summary>
+    Public Sub Reset()
+        turnLabel.Text = "PLAYER 1 [X] Turn"
+        For i = 0 To 8
+            If buttons(i).Image IsNot Nothing Then
+                buttons(i).Image = Nothing
+            End If
+        Next
+        player1Turn = True
+        player1Combinations.Clear()
+        player2Combinations.Clear()
+    End Sub
+    ''' <summary>
+    ''' changes main panel form to MainMenu form
+    ''' </summary>
+    Private Sub backButton_Click(sender As Object, e As EventArgs) Handles backButton.Click
+        Reset()
+        Module1.loadForm(MainMenu, Form1)
+    End Sub
+
     ''' <summary>
     ''' Make a play on a tile, or put a x and o in tile depending on whos
-    ''' turn it is
+    ''' turn it is, and store the row and column as integer as their combinations
+    ''' ex of row and column as short integer 
+    ''' is row 1 column 1 is 11; row2column3 is 23
+    ''' changes the text on the turn label
     ''' </summary>
     ''' <param name="but">button that is clicked</param>
     Public Sub Play(but As Button)
@@ -24,27 +54,66 @@ Public Class GameArea
             ''' Check whos turn
             If player1Turn Then
                 but.Image = My.Resources.xtile_gamearea1
+                ' this will remove the t in the name of button the name of button typicall is
+                ' t[row][column] so if i press t33, it will save to their combination as 33
+                player1Combinations.Add(CShort(but.Name.Replace("t", "")))
                 turnLabel.Text = "PLAYER 2 [O] Turn"
-                player1Turn = Not player1Turn
             Else
                 but.Image = My.Resources.otile_gamearea1
+                ' this will remove the t in the name of button the name of button typicall is
+                ' t[row][column] so if i press t33, it will save to their combination as 33
+                player2Combinations.Add(CShort(but.Name.Replace("t", "")))
                 turnLabel.Text = "PLAYER 1 [X] Turn"
-                player1Turn = Not player1Turn
             End If
         End If
     End Sub
 
     ''' <summary>
-    ''' changes main panel form to MainMenu form
+    ''' check if someone has won
     ''' </summary>
-    Private Sub backButton_Click(sender As Object, e As EventArgs) Handles backButton.Click
-        For i = 0 To 8
-            If buttons(i).Image IsNot Nothing Then
-                buttons(i).Image = Nothing
-            End If
-        Next
-        player1Turn = True
-        Module1.loadForm(MainMenu, Form1)
+    ''' <param name="player1Turn">if the move was from the player 1 or not</param>
+    ''' <returns>Return true or false if there was a winning combination inside
+    ''' player1Combinations and player2Combinations</returns>
+    Public Function WinCheck(player1Turn) As Boolean
+        Dim result As Boolean = True
+        If player1Turn Then
+            For i = 0 To 7
+                For j = 0 To 2
+                    If Not player1Combinations.Contains(winningCombination(i, j)) Then
+                        result = False
+                        Exit For
+                    End If
+                Next
+                If result Then
+                    Return result
+                Else
+                    result = True
+                End If
+            Next
+        Else
+            For i = 0 To 7
+                For j = 0 To 2
+                    If Not player2Combinations.Contains(winningCombination(i, j)) Then
+                        result = False
+                        Exit For
+                    End If
+                Next
+                If result Then
+                    Return result
+                Else
+                    result = True
+                End If
+            Next
+        End If
+        Return False
+    End Function
+
+    Public Sub MessageForWinner(player1Win)
+        If player1Win Then
+            MessageBox.Show("Player 1 Wins!")
+        Else
+            MessageBox.Show("Player 2 Wins!")
+        End If
     End Sub
 
     ''' <summary>
@@ -53,6 +122,13 @@ Public Class GameArea
     ''' </summary>
     Private Sub tiles_click(sender As Object, e As EventArgs) Handles t11.Click, t33.Click, t32.Click, t31.Click, t23.Click, t22.Click, t21.Click, t13.Click, t12.Click
         Play(DirectCast(sender, Button))
+        Dim someoneWin As Boolean = WinCheck(player1Turn)
+        If someoneWin Then
+            MessageForWinner(player1Turn)
+        Else
+            player1Turn = Not player1Turn
+        End If
+
     End Sub
 
 
