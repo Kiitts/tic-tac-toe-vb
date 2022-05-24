9b3a867fbd4891f21bb59da0a78542ea883d18ed
Johndell S. Kitts <k.jaydeee@gmail.com>
2022-05-24 19:19:26 +0800

    button image will now change if they clicked it depending who's turn to play, if player 1 it will show X, player 2 will show O, if the button clicked, has already an image, it will show an messagebox as an error. and let them choose other tile. when pressed back the tiles image will reset to nothing.

--- Tic-Tac-Toe/GameArea.vb
+++ Tic-Tac-Toe/GameArea.vb
@@ -1,9 +1,53 @@
-﻿Imports Tic_Tac_Toe.Module1
+﻿Imports Tic_Tac_Toe.Module1 ' module for changing main panel forms
+
+
 Public Class GameArea
+    Public player1Turn As Boolean = True
+    Public buttons() As Button
+    Private Sub GameArea_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        buttons = {t11, t12, t13, t21, t22, t23, t31, t32, t33}
+    End Sub
+    ''' <summary>
+    ''' Make a play on a tile, or put a x and o in tile depending on whos
+    ''' turn it is
+    ''' </summary>
+    ''' <param name="but">button that is clicked</param>
+    Public Sub Play(but As Button)
+        ''' Check if btn has already an image
+        If but.Image IsNot Nothing Then
+            MessageBox.Show("The tile is used already.")
+        Else
+            ''' Check whos turn
+            If player1Turn Then
+                but.Image = My.Resources.xtile_gamearea1
+                player1Turn = Not player1Turn
+            Else
+                but.Image = My.Resources.otile_gamearea1
+                player1Turn = Not player1Turn
+            End If
+        End If
+    End Sub
+
     ''' <summary>
     ''' changes main panel form to MainMenu form
     ''' </summary>
     Private Sub backButton_Click(sender As Object, e As EventArgs) Handles backButton.Click
+        For i = 0 To 8
+            If buttons(i).Image IsNot Nothing Then
+                buttons(i).Image = Nothing
+            End If
+        Next
+        player1Turn = True
         Module1.loadForm(MainMenu, Form1)
     End Sub
+
+    ''' <summary>
+    ''' This will trigger if any tile is clicked
+    ''' When any play tile is clicked
+    ''' </summary>
+    Private Sub tiles_click(sender As Object, e As EventArgs) Handles t11.Click, t33.Click, t32.Click, t31.Click, t23.Click, t22.Click, t21.Click, t13.Click, t12.Click
+        Play(DirectCast(sender, Button))
+    End Sub
+
+
 End Class
--- Tic-Tac-Toe/My Project/Resources.Designer.vb
+++ Tic-Tac-Toe/My Project/Resources.Designer.vb
@@ -83,6 +83,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property otile_gamearea1() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("otile_gamearea1", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property playbutton_mainmenu() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("playbutton_mainmenu", resourceCulture)
@@ -96,6 +106,16 @@ Namespace My.Resources
         Friend ReadOnly Property xtile_gamearea() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("xtile_gamearea", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property xtile_gamearea1() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("xtile_gamearea1", resourceCulture)
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
